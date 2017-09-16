bb310079375e11e4232613c4a9852787b0bad3ee
domrev_igor <igor@innovid.com>
2017-09-16 16:48:31 +0300

    snapshot testing

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,25 @@
 const path = require('path');
 module.exports = {
-  entry: ['babel-polyfill','./lib/components/Index.jsx'],
+  entry: ['babel-polyfill', './lib/components/Index.jsx'],
+  entry: ['babel-polyfill', './lib/components/Index.jsx'],
   output: {
     path: path.resolve(__dirname, 'public'),
     filename: 'bundle.js'
   },
+  resolve: {
+    extensions: ['.js', '.jsx', '.json','.scss']
+  },
   module: {
     loaders: [
-      { test: /\.(jsx|js)$/, loader: 'babel-loader', exclude: /node_modules/ }
+      { test: /\.(jsx|js)$/, loader: 'babel-loader', exclude: /node_modules/ },
+      {
+        test: /\.scss$/,
+        use: [
+          { loader: "style-loader" },
+          { loader: "css-loader" },
+          { loader: "sass-loader" }
+        ]
+      }
     ]
   }
 };
