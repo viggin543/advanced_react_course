a414ac02341df5cf571e97e3131c547ff94f5e23
domrev_igor <igor@innovid.com>
2017-09-18 21:18:56 +0300

    state-api sub module

--- webpack.config.js
+++ webpack.config.js
@@ -2,26 +2,30 @@ const path = require('path');
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = {
-  entry: ['./lib/components/App.scss','babel-polyfill','./lib/renderers/dom.jsx',],
+  entry: ['./lib/components/App.scss',
+    'babel-polyfill', './lib/renderers/dom.jsx',],
   output: {
     path: path.resolve(__dirname, 'public'),
     filename: 'bundle.js'
   },
   resolve: {
-    modules:['lib','node_modules'],
+    modules: ['lib', 'node_modules'],
     extensions: ['.js', '.jsx', '.json', '.scss']
   },
   module: {
     rules: [
-      { test: /\.(jsx|js)$/, loader: 'babel-loader', exclude: /node_modules/ },
+      {
+        test: /\.(jsx|js)$/, loader: 'babel-loader',
+        exclude: /node_modules/
+      },
       {
         test: /\.scss$/,
         use: ExtractTextPlugin.extract([
-           "css-loader", "sass-loader"
+          "css-loader", "sass-loader"
         ]),
         exclude: /node_modules/
       },
     ]
   },
-  plugins: [new ExtractTextPlugin('style.css')]  
+  plugins: [new ExtractTextPlugin('style.css')]
 };
