45e323bfbf00bf7fa2034d8028fbbb367bd136cb
domrev_igor <igor@innovid.com>
2017-09-20 16:31:43 +0300

    added vanila store to the application

--- lib/renderers/dom.jsx
+++ lib/renderers/dom.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import App from 'components/App'
+import Api from 'state-api';
+
+const store =  new Api(window.initialData);
 
 ReactDom.render(
-  <App initialData={window.initialData}/>,
+  <App store={store}/>,
   document.getElementById('root')
 );
