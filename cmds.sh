037f5f219bf335a0af5415a3f1017e75867ff5d8
domrev_igor <igor@innovid.com>
2017-09-21 19:47:45 +0300

    cleaning up b4 production build changes

--- cmds.sh
+++ cmds.sh
@@ -8,5 +8,19 @@ yarn add --dev babel-eslint
 yarn dev # run server
 yarn pm2 logs  # see dev server logs
 yarn pm2 kill # guess what ..
-yarn webpacl
+yarn webpaack -wd
 yarn upgrade-interactive
+yarn jest --coverage
+yarn jest test --watch
+yarn NODE_PATH=./lib pm2 start lib/server.js --watch --interpreter babel-node
+
+
+ "babel": {
+    "presets": [
+      "react",
+      "env",
+      "stage-2"
+    ]
+  },
+
+    "stage-2"  : this one says babel to take javascript version including all the polyfills
